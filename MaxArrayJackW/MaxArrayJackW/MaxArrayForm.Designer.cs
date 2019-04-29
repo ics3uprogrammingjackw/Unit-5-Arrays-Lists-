f99f014d2a1ecab583be0f92765befa01cff84cf
JackWrightMac <32547204+JackWrightMac@users.noreply.github.com>
2019-04-29 13:00:39 -0400

    Initial commit

---
+++ MaxArrayJackW/MaxArrayJackW/MaxArrayForm.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace MaxArrayJackW
+{
+    partial class frmMaxArray
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lstNumbers = new System.Windows.Forms.ListBox();
+            this.lblMaxValue = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(159, 50);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lstNumbers
+            // 
+            this.lstNumbers.FormattingEnabled = true;
+            this.lstNumbers.Location = new System.Drawing.Point(119, 100);
+            this.lstNumbers.Name = "lstNumbers";
+            this.lstNumbers.Size = new System.Drawing.Size(159, 147);
+            this.lstNumbers.TabIndex = 1;
+            // 
+            // lblMaxValue
+            // 
+            this.lblMaxValue.AutoSize = true;
+            this.lblMaxValue.Location = new System.Drawing.Point(156, 282);
+            this.lblMaxValue.Name = "lblMaxValue";
+            this.lblMaxValue.Size = new System.Drawing.Size(90, 13);
+            this.lblMaxValue.TabIndex = 2;
+            this.lblMaxValue.Text = "The max value is:";
+            // 
+            // frmMaxArray
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 350);
+            this.Controls.Add(this.lblMaxValue);
+            this.Controls.Add(this.lstNumbers);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmMaxArray";
+            this.Text = "Max Array by Jack W";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox lstNumbers;
+        private System.Windows.Forms.Label lblMaxValue;
+    }
+}
+
---
+++ MaxArrayJackW/MaxArrayJackW/MaxArrayForm.cs
@@ -0,0 +1,87 @@
+﻿/*
+ * Created by: Jack Wright 
+ * Created on: 26-04-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #37 - Find Max Value
+ * This program finds the max value of all the numbers in an array
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MaxArrayJackW
+{
+    public partial class frmMaxArray : Form
+    {
+        public frmMaxArray()
+        {
+            InitializeComponent();
+        }
+
+        private int GetMaxValue(int[] tmpArrayOfIntegers)
+        {
+            //declaring variables and constants
+            int tmpMaxValue = 0;
+            int counter;
+            const int MAX_ARRAY_SIZE = 10;
+            
+
+            for (counter = 0; counter < MAX_ARRAY_SIZE; counter++)
+            {
+
+                if (tmpArrayOfIntegers[counter] > tmpMaxValue)
+                {
+                    //Sets the current max value to the current index if it is greater than the last max value
+                    tmpMaxValue = tmpArrayOfIntegers[counter];
+                }
+            }
+
+            //return the max value 
+            return tmpMaxValue;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //Declaring constants and variables
+            const int MAX_ARRAY_SIZE = 10;
+            const int MAX_RANDOM_NUMBER = 500;
+            int[] arrayOfInt = new int[MAX_ARRAY_SIZE];
+            int counter, randomNumber, maxValue;
+            Random randomNumberGenerator = new Random();
+
+            //Clears the listbox
+            lstNumbers.Items.Clear();
+
+
+            for (counter = 0; counter < MAX_ARRAY_SIZE; counter++)
+            {
+                //generate a random number
+                randomNumber = randomNumberGenerator.Next(1, MAX_RANDOM_NUMBER + 1);
+
+                //Setting the current index of the array to the generated number
+                arrayOfInt[counter] = randomNumber;
+
+                //Add the number to the listbox
+                this.lstNumbers.Items.Add(randomNumber);
+
+                //refresh the form
+                this.Refresh();
+                
+            }
+
+            //call the GetMaxValue function
+            maxValue = GetMaxValue(arrayOfInt);
+
+            //Display the max value
+            lblMaxValue.Text = "The max value is: " + maxValue;
+
+        }
+    }
+}
