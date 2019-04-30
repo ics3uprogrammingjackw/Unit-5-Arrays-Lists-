7305f5f380be091fd03921c137ca4ab084cd0c24
JackWrightMac <32547204+JackWrightMac@users.noreply.github.com>
2019-04-30 13:02:10 -0400

    Min array finished

---
+++ MinArrayJackW/MaxArrayJackW/MinArrayForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace MaxArrayJackW
+{
+    partial class frmMinArray
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lstNumbers = new System.Windows.Forms.ListBox();
+            this.lblMinValue = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(159, 50);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lstNumbers
+            // 
+            this.lstNumbers.FormattingEnabled = true;
+            this.lstNumbers.Location = new System.Drawing.Point(119, 100);
+            this.lstNumbers.Name = "lstNumbers";
+            this.lstNumbers.Size = new System.Drawing.Size(159, 147);
+            this.lstNumbers.TabIndex = 1;
+            // 
+            // lblMinValue
+            // 
+            this.lblMinValue.AutoSize = true;
+            this.lblMinValue.Location = new System.Drawing.Point(156, 282);
+            this.lblMinValue.Name = "lblMinValue";
+            this.lblMinValue.Size = new System.Drawing.Size(87, 13);
+            this.lblMinValue.TabIndex = 2;
+            this.lblMinValue.Text = "The min value is:";
+            // 
+            // frmMinArray
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 350);
+            this.Controls.Add(this.lblMinValue);
+            this.Controls.Add(this.lstNumbers);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmMinArray";
+            this.Text = "Min Array by Jack W";
+            this.Load += new System.EventHandler(this.frmMinArray_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox lstNumbers;
+        private System.Windows.Forms.Label lblMinValue;
+    }
+}
+
---
+++ MinArrayJackW/MaxArrayJackW/MinArrayForm.cs
@@ -0,0 +1,95 @@
+﻿/*
+ * Created by: Jack Wright 
+ * Created on: 30-04-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #38 - Find Min Value
+ * This program finds the min value of all the numbers in an array
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MaxArrayJackW
+{
+    public partial class frmMinArray : Form
+    {
+        public frmMinArray()
+        {
+            InitializeComponent();
+        }
+
+        private int GetMinValue(int[] tmpArrayOfIntegers)
+        {
+            //declaring variables and constants
+            int tmpMinValue = 501;
+            int counter = 0;
+            
+
+            foreach (double arrayValue in tmpArrayOfIntegers)
+            {
+                
+                //If the current value in the array is less than the current minimum value 
+                if (tmpArrayOfIntegers[counter] < tmpMinValue)
+                {
+                    //Sets the current min value to the current index if it is less than the last min value
+                    tmpMinValue = tmpArrayOfIntegers[counter];
+                }
+
+                //Increment the counter 
+                counter++;
+            }
+
+            //return the min value 
+            return tmpMinValue;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //Declaring constants and variables
+            const int MAX_ARRAY_SIZE = 10;
+            const int MAX_RANDOM_NUMBER = 500;
+            int[] arrayOfInt = new int[MAX_ARRAY_SIZE];
+            int counter, randomNumber, minValue;
+            Random randomNumberGenerator = new Random();
+
+            //Clears the listbox
+            lstNumbers.Items.Clear();
+
+
+            for (counter = 0; counter < MAX_ARRAY_SIZE; counter++)
+            {
+                //generate a random number
+                randomNumber = randomNumberGenerator.Next(1, MAX_RANDOM_NUMBER + 1);
+
+                //Setting the current index of the array to the generated number
+                arrayOfInt[counter] = randomNumber;
+
+                //Add the number to the listbox
+                this.lstNumbers.Items.Add(randomNumber);
+
+                //refresh the form
+                this.Refresh();
+                
+            }
+
+            //call the GetMinValue function
+            minValue = GetMinValue(arrayOfInt);
+
+            //Display the min value
+            lblMinValue.Text = "The min value is: " + minValue;
+
+        }
+
+        private void frmMinArray_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
