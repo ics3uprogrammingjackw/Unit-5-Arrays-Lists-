d9f0e20899bfdcbf30024fbc9ca43a7e9c0310ee
JackWrightMac <32547204+JackWrightMac@users.noreply.github.com>
2019-05-01 13:08:29 -0400

    Read Write

---
+++ ReadWriteJackW/ReadWriteJackW/ReadWriteForm.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace ReadWriteJackW
+{
+    partial class frmReadWrite
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblStatement = new System.Windows.Forms.Label();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblStatement
+            // 
+            this.lblStatement.AutoSize = true;
+            this.lblStatement.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.lblStatement.Location = new System.Drawing.Point(56, 20);
+            this.lblStatement.Name = "lblStatement";
+            this.lblStatement.Size = new System.Drawing.Size(364, 66);
+            this.lblStatement.TabIndex = 0;
+            this.lblStatement.Text = "Compare strings on each line of input.txt. file\r\n\r\n(Case doesn\'t matter)";
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(182, 155);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(75, 23);
+            this.btnCheck.TabIndex = 1;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.lblOutput.Location = new System.Drawing.Point(56, 279);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(316, 22);
+            this.lblOutput.TabIndex = 2;
+            this.lblOutput.Text = "Output has been generated to text file.";
+            // 
+            // frmReadWrite
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 355);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.lblStatement);
+            this.Name = "frmReadWrite";
+            this.Text = "Read & Write by Jack W";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStatement;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Label lblOutput;
+    }
+}
+
---
+++ ReadWriteJackW/ReadWriteJackW/ReadWriteForm.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReadWriteJackW
+{
+    public partial class frmReadWrite : Form
+    {
+        public frmReadWrite()
+        {
+            InitializeComponent();
+        }
+
+        private bool StringsAreEqual(string case1, string case2)
+        {
+            //Creating boolean variable
+            bool isEqual;
+
+            //Determines whether the two strings are equal in length
+            if (case1.Length == case2.Length)
+            {
+                //Converting strings to upper case
+                case1 = case1.ToUpper();
+                case2 = case2.ToUpper();
+
+                if (case1 == case2)
+                {
+                    //If both strings are equal to each other the boolean variable is true 
+                    isEqual = true;
+                }
+                else
+                {
+                    //If both strings are not equal to each other the boolean variable is false
+                    isEqual = false;
+                }
+            }
+            else
+            {
+                //If the length is not equal the boolean variable is false
+                isEqual = false;
+            }
+            //Returns the boolean variable
+            return isEqual;
+
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"input.txt");
+
+            char[] charSeperators = new char[] {' ', '\t' };
+
+            string output = "";
+
+            foreach (string line in lines)
+            {
+                string[] words = line.Split(charSeperators, StringSplitOptions.RemoveEmptyEntries);
+
+                if (StringsAreEqual(words[0], words[1]) == true)
+                {
+                    output = output + "True\r\n";
+
+                }
+                else
+                {
+                    output = output + "False\r\n";
+                }
+            }
+
+            System.IO.File.WriteAllText(@"output.txt", output);
+
+            this.lblOutput.Show();
+
+        }
+    }
+}
